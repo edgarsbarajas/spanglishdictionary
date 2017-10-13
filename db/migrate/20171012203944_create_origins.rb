16c7bb8fb838ec958dfe5208ab42b2e13b487f75
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-12 18:37:37 -0700

    Able to create users, defined words, comment and vote

--- db/migrate/20171012203944_create_origins.rb
+++ db/migrate/20171012203944_create_origins.rb
@@ -1,6 +1,7 @@
 class CreateOrigins < ActiveRecord::Migration[5.1]
   def change
     create_table :origins do |t|
+      t.string :country, unique: true, null: false
 
       t.timestamps
     end
--- db/migrate/20171012204116_create_votes.rb
+++ db/migrate/20171012204116_create_votes.rb
@@ -1,6 +1,10 @@
 class CreateVotes < ActiveRecord::Migration[5.1]
   def change
     create_table :votes do |t|
+      t.references :user, null: false
+      t.references :voteable, polymorphic: true, index: true, null: false
+      t.integer    :value, null: false
+      t.boolean    :upvote, null: false
 
       t.timestamps
     end
