8c7765c5b41700660ae1031f353a170b84c33e07
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-12 19:27:15 -0700

    Users are able to define words, vote, and comment. All associations are linked together correctly. Some cool stuff is about to go down.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
 
+  validates :user, :commentable, :text, presence: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,6 @@
 class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :voteable, polymorphic: true
+
+  validates :user, :voteable, :value, :upvote, presence: true
 end
