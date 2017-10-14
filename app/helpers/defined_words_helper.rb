f9f58f0c8b57227af41b84ba9a67203bc2413685
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-13 21:56:43 -0700

    Users are now able to add words. No user auth yet. Still need to add the eaxample field.

---
+++ app/helpers/defined_words_helper.rb
@@ -0,0 +1,2 @@
+module DefinedWordsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :defined_words, only: [:create]
+  resources :defined_words, only: [:new, :create]
 end
