668916eaed3b346e368b9e5025b141b698c40152
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-15 20:28:28 -0700

    Seed the defined words and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+DefinedWords.destroy_all
+
+
+20.times do {
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::RickAndMorty.character, gender: ["Male", "Female"].sample, email: Faker::Internet.unique.email, password: "booboo", dob: Faker::Date.birthday(18, 65))
+}
+
+200.times do {
+  DefinedWords.create(word: Faker::SiliconValley.company, definition: Faker::SiliconValley.quote,user: User.all.sample, origin: Origin.all.sample, example: Faker::RickAndMorty.quote)
+}
