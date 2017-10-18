0f36e88b2ab20f504dfeb7791b1e026f024c7552
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-17 23:50:08 -0700

    Add link to defined words partial to reply / add a comment to it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :defined_words, only: [:new, :create] do
     resources :votes, only: [:create]
-  end
-
-  resources :comments, only: [:new, :create] do
-    resources :votes, only: [:create]
+    resources :comments, only: [:new, :create]
   end
 
   resources :users, only: [] do
