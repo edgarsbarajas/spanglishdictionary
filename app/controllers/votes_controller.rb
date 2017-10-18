2d4d2822f00bfce7bb149e8de07924efa564427f
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-17 21:30:55 -0700

    Users can only upvote a word once, and the dislike is removed if it exists when upvoting.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,9 +4,17 @@ class VotesController < ApplicationController
   def create
     if params["defined_word_id"]
       if params["commit"] == "Upvote"
-        p Vote.create(user: current_user, voteable_id: params["defined_word_id"], voteable_type: "DefinedWord", value: 1, upvote: true)
+        # if upvote doesnt exist
+        if !Vote.where(user: current_user, voteable_type: "DefinedWord", voteable_id: params["defined_word_id"], upvote: true).exists?
+          # create it
+          Vote.create(user: current_user, voteable_id: params["defined_word_id"], voteable_type: "DefinedWord", value: 1, upvote: true)
+          # delete the downvote on the word from that user if it exists
+          if downvote = Vote.find_by(user: current_user, voteable_id: params["defined_word_id"], voteable_type: "DefinedWord", upvote: false)
+            downvote.delete
+          end
+        end
       elsif params["commit"] == "Downvote"
-        p Vote.create(user: current_user, voteable_id: params["defined_word_id"], voteable_type: "DefinedWord", value: 1, upvote: false)
+        Vote.create(user: current_user, voteable_id: params["defined_word_id"], voteable_type: "DefinedWord", value: 1, upvote: false)
       end
     elsif params["comment_id"]
 
@@ -15,3 +23,8 @@ class VotesController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 end
+
+# if defined_word_vote
+  # clicked upvote
+    # if upvote exists, ignore
+    # if upvote doesnt exist, create it, and delete the downvote from that user if it exists
