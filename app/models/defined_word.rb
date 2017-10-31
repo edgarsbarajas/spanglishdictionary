491636a9be6938637465eb75325e780c1f06cff2
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-10-31 12:14:58 -0700

    Delete the origins model and its associations + validations

--- app/models/defined_word.rb
+++ app/models/defined_word.rb
@@ -1,10 +1,9 @@
 class DefinedWord < ApplicationRecord
   belongs_to :user
-  belongs_to :origin
   has_many :votes, as: :voteable
   has_many :comments, as: :commentable
 
-  validates :user, :origin, :word, :definition, presence: true
+  validates :user, :word, :definition, presence: true
 
   def has_comment?(comment)
     self.comments.where(id: comment.id).count > 0
