d4f9703befc3e9d01b9fa76ce52750e4cee02fb6
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-11-01 22:54:30 -0700

    Downcase any future defined words up being submitted

--- app/models/defined_word.rb
+++ app/models/defined_word.rb
@@ -5,6 +5,12 @@ class DefinedWord < ApplicationRecord
 
   validates :user, :word, :definition, presence: true
 
+  before_save :downcase_fields
+
+  def downcase_fields
+    self.word.downcase
+  end
+
   def has_comment?(comment)
     self.comments.where(id: comment.id).count > 0
   end
